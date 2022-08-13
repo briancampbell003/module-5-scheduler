1aec937cb1271bfc8f9998ad78dc4b0605bdd866
Brian Campbell <brian.campbell003@gmail.com>
2022-08-13 16:55:15 -0500

    Finished local storage save and print

--- script.js
+++ script.js
@@ -1,121 +1,122 @@
+// Use dayjs to display today's date
 let today = dayjs().format("dddd, MM/DD/YYYY");
 $("#currentDay").text(today);
-
-let hour = dayjs().hour();
 
 // Call previous entries from local storage
 let oldEntries = JSON.parse(localStorage.getItem("SavedItems")) || [];
 for (let i = 0; i < oldEntries.length; i++) {
-    // let populateMe = oldEntries[i].row.children(1).children().children();
+    // Set variables holding data for saved events
     let rowDiv = oldEntries[i].row;
-    // $("rowDiv").children(1).children(1).text = oldEntries[i].text;
-    $("rowDiv").children(1).children(1).text = "Hello!";
+    let savedText = oldEntries[i].text;
 
-    console.log(oldEntries);
+    // Populate block with matching saved text
+    $("#"+rowDiv).children(1).children(1).children().val(savedText);
+    
 }
 
-// Save values with button using event target logic
+// Save new values with button using event target logic
 let saveBtn = $(".saveBtn");
 saveBtn.on('click', function( event ) {
     event.preventDefault();
+    // Use jQuery logic to capture rowid and corresponding event to save
     let targetRow = $(event.target).parent().parent();
     let textToSave = targetRow.children(1).children().children().val();
     let rowAndText = {row: targetRow.attr("id"), text: textToSave};
+    
+    // Create and store array with saved items
     let arrayToSave = JSON.parse(localStorage.getItem("SavedItems")) || [];
     arrayToSave.push(rowAndText);
     localStorage.setItem("SavedItems", JSON.stringify(arrayToSave));
 
-    console.log(targetRow);
-    console.log("something");
-    console.log(textToSave);
 });
 
 
 
 // Add color displays based on current time
-if (hour < 8) {
+let hour = dayjs().hour();
+if (hour > 8) {
     $("#8am-block").addClass("bg-light");
-} else if (hour > 8) {
+} else if (hour < 8) {
     $("#8am-block").addClass("bg-success");
 } else {
     $("#8am-block").addClass("bg-danger");
 }
 
-if (hour < 9) {
+if (hour > 9) {
     $("#9am-block").addClass("bg-light");
-} else if (hour > 9) {
+} else if (hour < 9) {
     $("#9am-block").addClass("bg-success");
 } else {
     $("#9am-block").addClass("bg-danger");
 }
 
-if (hour < 10) {
+if (hour > 10) {
     $("#10am-block").addClass("bg-light");
-} else if (hour > 10) {
+} else if (hour < 10) {
     $("#10am-block").addClass("bg-success");
 } else {
     $("#10am-block").addClass("bg-danger");
 }
 
-if (hour < 11) {
+if (hour > 11) {
     $("#11am-block").addClass("bg-light");
-} else if (hour > 11) {
+} else if (hour < 11) {
     $("#11am-block").addClass("bg-success");
 } else {
     $("#11am-block").addClass("bg-danger");
 }
 
-if (hour < 12) {
+if (hour > 12) {
     $("#12pm-block").addClass("bg-light");
-} else if (hour > 12) {
+} else if (hour < 12) {
     $("#12pm-block").addClass("bg-success");
 } else {
     $("#12pm-block").addClass("bg-danger");
 }
 
-if (hour < 13) {
-    $("#1pm-block").addClass("bg-info");
-} else if (hour > 13) {
+if (hour > 13) {
+    $("#1pm-block").addClass("bg-light");
+} else if (hour < 13) {
     $("#1pm-block").addClass("bg-success");
 } else {
     $("#1pm-block").addClass("bg-danger");
 }
 
-if (hour < 14) {
+if (hour > 14) {
     $("#2pm-block").addClass("bg-light");
-} else if (hour > 14) {
+} else if (hour < 14) {
     $("#2pm-block").addClass("bg-success");
 } else {
     $("#2pm-block").addClass("bg-danger");
 }
 
-if (hour < 15) {
+if (hour > 15) {
     $("#3pm-block").addClass("bg-light");
-} else if (hour > 15) {
+} else if (hour < 15) {
     $("#3pm-block").addClass("bg-success");
 } else {
     $("#3pm-block").addClass("bg-danger");
 }
 
-if (hour < 16) {
+if (hour > 16) {
     $("#4pm-block").addClass("bg-light");
-} else if (hour > 16) {
+} else if (hour < 16) {
     $("#4pm-block").addClass("bg-success");
 } else {
     $("#4pm-block").addClass("bg-danger");
 }
 
-if (hour < 17) {
+if (hour > 17) {
     $("#5pm-block").addClass("bg-light");
-} else if (hour > 17) {
+} else if (hour < 17) {
     $("#5pm-block").addClass("bg-success");
 } else {
     $("#5pm-block").addClass("bg-danger");
 }
 
-if (hour < 18) {
+if (hour > 18) {
     $("#6pm-block").addClass("bg-light");
-} else if (hour > 18) {
+} else if (hour < 18) {
     $("#6pm-block").addClass("bg-success");
 } else {
     $("#6pm-block").addClass("bg-danger");
